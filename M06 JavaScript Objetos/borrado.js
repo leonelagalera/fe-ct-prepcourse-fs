602dd1e49d823775df5b909091ce13b8a3f64f92
leonelagalera <galeraleonela@gmail.com>
2023-08-03 16:57:24 -0300

    hw 06

---
+++ M06 JavaScript Objetos/borrado.js
@@ -0,0 +1,11 @@
+function actualizarPassword(objetoUsuario, nuevaPassword) {
+    // Reemplaza la contrseña guardada en la propiedad "password" del "objetoUsuario".
+    // La nueva contraseña la recibes por parámetro.
+    // Retornar el objeto.
+    // Tu código:
+    var objetoUsuario= {password}
+    objetoUsuario.password=nuevaPassword;
+    
+}
+
+console.log(objetoUsuario)
