c379a1d2466db62f8a66a9c29db926809e49a6a6
leonelagalera <galeraleonela@gmail.com>
2023-08-03 23:12:51 -0300

    hw 7

--- M06 JavaScript Objetos/borrado.js
+++ M06 JavaScript Objetos/borrado.js
@@ -1,11 +1,15 @@
-function actualizarPassword(objetoUsuario, nuevaPassword) {
-    // Reemplaza la contrseña guardada en la propiedad "password" del "objetoUsuario".
-    // La nueva contraseña la recibes por parámetro.
-    // Retornar el objeto.
-    // Tu código:
-    var objetoUsuario= {password}
-    objetoUsuario.password=nuevaPassword;
+function agregarStringInvertida() {
+    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
+    // El método debe llamarse "reverse".
+    // [PISTA]: necesitarás utilizar el objeto "this".
     
-}
+    String.prototype.reverse= function(){
+       return this.split(" ").reverse( ).join(" ");
+    } 
+    const palabra = "Hola";
+    const palabraInvertida = palabra.reverse();
+    console.log(palabraInvertida); 
+ }
 
-console.log(objetoUsuario)
+
+ 
